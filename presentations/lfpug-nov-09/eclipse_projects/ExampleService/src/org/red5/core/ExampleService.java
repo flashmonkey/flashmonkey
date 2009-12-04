cd365c6fe29390cfce813ace56cb7d9bbf14e6de
Trevor Burton <trevor@infrared5.com>
2009-12-04 13:31:51 +0000

    added the eclipse project files from the lfpug preso

---
+++ presentations/lfpug-nov-09/eclipse_projects/ExampleService/src/org/red5/core/ExampleService.java
@@ -0,0 +1,12 @@
+package org.red5.core;
+
+public class ExampleService {
+
+	public ExampleService() {
+		System.out.println("ExampleService starting");
+	}
+	
+	public int add(int a, int b) {
+		return a + b;
+	}
+}
